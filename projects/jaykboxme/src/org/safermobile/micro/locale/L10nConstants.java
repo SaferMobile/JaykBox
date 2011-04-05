80fc6aec3efb41d8f62dc8212e154fb8f282bdc2
Nathan Freitas <nathan@freitas.net>
2011-04-05 14:18:41 -0400

    Adding in J2ME tester app

---
+++ projects/jaykboxme/src/org/safermobile/micro/locale/L10nConstants.java
@@ -0,0 +1,20 @@
+package org.safermobile.micro.locale;
+/**
+ *	L10nConstants class is auto generated. DO NOT CHANGE IT.
+ */
+public class L10nConstants {
+	
+    /**
+ 	 *	LOCALE NAMES
+ 	 */
+    public class locales {
+
+    }
+
+    /**
+ 	 *	KEY VALUES
+ 	 */    
+    public class keys {
+
+    }
+}
